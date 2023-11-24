47c28d7170cd4929067ce80e889be74a51ca9400
Evi Vermeêren <evivermeeren@hotmail.com>
2023-11-24 15:47:10 +0100

    feat: Implement real-time score updates in ScoreUpdater.vue

--- src/components/ScoreUpdater.vue
+++ src/components/ScoreUpdater.vue
@@ -1,7 +1,42 @@
-<script></script>
+<!-- UpdateScores.vue -->
+<script setup>
+import { ref, onMounted } from "vue";
+import { scoreboardStore } from "../components/store";
+import { websocketStore } from "../components/store";
+
+const selectedOption = ref("option1");
+const score = ref("");
+
+const updateScore = () => {
+  const teamName = `Team ${selectedOption.value}`;
+  const newScore = parseInt(score.value) || 0;
+
+  // Assuming you have a server endpoint that accepts score updates
+  websocketStore.socket.send(
+    JSON.stringify({ team: teamName, score: newScore })
+  );
+};
+
+onMounted(() => {
+  websocketStore.connect();
+});
+</script>
 
 <template>
-  <div><p>TEST</p></div>
+  <div>
+    <h1>Update scores</h1>
+
+    <select v-model="selectedOption">
+      <option value="option1">Team 1</option>
+      <option value="option2">Team 2</option>
+    </select>
+
+    <div>
+      <p>Score:</p>
+      <input v-model="score" type="text" />
+      <button @click="updateScore">Update Score</button>
+    </div>
+  </div>
 </template>
 
 <style scoped></style>
