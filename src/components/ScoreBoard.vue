7ba0e14863d070bd0b29ab305cfb81d90636b9d1
Evi Vermeêren <evivermeeren@hotmail.com>
2023-11-24 15:18:19 +0100

    feat: add scoreboard layout

--- src/components/ScoreBoard.vue
+++ src/components/ScoreBoard.vue
@@ -1,5 +1,49 @@
-<script setup></script>
+<script setup>
+import { ref } from "vue";
 
-<template></template>
+const teams = ref([
+  { name: "Team A", score: 20 },
+  { name: "Team B", score: 15 },
+]);
+</script>
 
-<style scoped></style>
+<template>
+  <div>
+    <table class="team-table">
+      <thead>
+        <tr>
+          <th>Team</th>
+          <th>Score</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(team, index) in teams" :key="index">
+          <td>{{ team.name }}</td>
+          <td>{{ team.score }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<style scoped>
+.team-table {
+  width: 100%;
+  border-collapse: collapse;
+  margin-top: 20px;
+}
+.team-table th,
+.team-table td {
+  border: 1px solid #ddd;
+  padding: 8px;
+  text-align: left;
+}
+
+.team-table th {
+  background-color: #f2f2f2;
+}
+
+.team-table tr:hover {
+  background-color: #f5f5f5;
+}
+</style>
